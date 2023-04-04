c14ec4a88107a743b8802220e309fcea9f85fab7
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-04-04 20:10:10 +0200

    Create project01

---
+++ 01-LittleGameCore/Assets/Scripts/EventReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class EventReceiver : MonoBehaviour
+    {
+        public event Action OnEvent;
+
+        public void Call()
+        {
+            OnEvent?.Invoke();
+        }
+    }
+}
---
+++ 01-LittleGameCore/Assets/Scripts/IntBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public sealed class IntBehaviour: MonoBehaviour
+    {
+        [SerializeField] private int value;
+
+        public event Action<int> OnValueChanged; 
+
+        public int Value
+        {
+            get => value;
+            set
+            {
+                this.value = value; 
+                OnValueChanged?.Invoke(value);
+            }
+        }
+    }
+}
